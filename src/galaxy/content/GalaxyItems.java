c5c45e94e17acdbb2c1d35762d259abe16ca1951
putri1244 <ijakamiaku@gmail.com>
2025-02-07 06:07:32 +0700

    update

--- src/galaxy/content/GalaxyItems.java
+++ src/galaxy/content/GalaxyItems.java
@@ -2,17 +2,24 @@ package galaxy.content;
 
 import arc.graphics.*;
 import mindustry.*;
+import galaxy.graphics.*;
+import galaxy.type.item.*;
 import mindustry.content.*;
 import mindustry.type.*;
 import mindustry.world.meta.*;
+
 
 public class GalaxyItems {
     public static Item galaxrium;
     
     public static void load() {
-        galaxrium = new Item("galaxrium", Color.valueOf("#cc45f6")) {{
-            hardness = 2;
-            cost = 1f;
+        galaxrium = new AnimatedItem("galaxrium", Color.valueOf("00ffff")) {{
+            hardness = 15;
+            cost = 0.1f;
+            charge = 10.24f;
+            radioactivity = 10.24f;
+            transition = 5;
+            animDelay = 4f;
             // Properties lain
         }};
     }
